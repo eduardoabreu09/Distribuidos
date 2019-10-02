c865aa75cee65eaf7e023d29da73021af31cdb61
eduardoabreu <eduardoabreu@alu.ufc.br>
2019-10-02 16:41:53 -0300

    App header

--- App.js
+++ App.js
@@ -1,55 +1,82 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'native-base'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, Container, Header, Title, Body, Left, Content} from 'native-base'
+import { Icon } from 'react-native-elements';
 
 const sensor_pb = require('./src/util/sensor_pb.js');
 export default class App extends Component{
   constructor(props) {
     super(props);
     this.state = { 
-      sensor: new sensor_pb.Sensor()
+      sensor: new sensor_pb.Sensor(),
+      disabled: false
      };
   };
 
   _setState = (num) => {
     this.state.sensor.setState(num);
     let sensor = this.state.sensor;
+    console.log(sensor.serializeBinary())
     this.setState({sensor})
   }
 
   _resetState = () => {
     this.state.sensor.setState(0);
     let sensor = this.state.sensor;
+    console.log(sensor.serializeBinary())
     this.setState({sensor})
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Teste do objeto</Text>
-        <Button 
-          style = {styles.button}
-          onPress = { () => { this._setState(10) }}>
-          <Text style = {styles.buttonText}>Clicar</Text>
-        </Button>
-        <Text style={styles.instructions}>Medição do sensor</Text>
-        <Text>{ this.state.sensor.getState() }</Text>
-        <Button 
-          style = {styles.buttonReset}
-          onPress = { () => { this._resetState() }}>
-          <Text style = {styles.buttonText}>Reset</Text>
-        </Button>
-      </View>
+      <Container>
+        <Header style={styles.header}>
+          <Left>
+            <TouchableOpacity 
+            style ={styles.iconTouch}
+            onPress= { () => alert("Conectar") }>
+              <Icon name="device-hub"
+              color = "#fff"/>
+            </TouchableOpacity>
+          </Left>
+          <Body style={styles.body} >
+            <Title>
+              Sensores.io
+            </Title>
+          </Body>
+        </Header>
+        <Content style ={styles.content}>
+          <Button 
+            style = {styles.button}
+            disabled = {this.state.disabled}
+            onPress = { () => { this._setState(10) }}>
+            <Text style = {styles.buttonText}>Clicar</Text>
+          </Button>
+          <Text style={styles.instructions}>Medição do sensor</Text>
+          <Text>{ this.state.sensor.getState() }</Text>
+          <Button 
+            style = {styles.buttonReset}
+            onPress = { () => { this._resetState() }}>
+            <Text style = {styles.buttonText}>Reset</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  header: {
+    alignItems:'center'
+  },
+  body:{
+    marginLeft:35
+  },
+  content:{
+    marginTop:10
+  },
+  iconTouch:{
+    padding:20
   },
   welcome: {
     fontSize: 20,
